f41beb0f7eb31781ffcf5b07a696d1d8de013d93
Kevin Bowman <bowman290@gmail.com>
2025-08-19 16:13:39 +0100

    refactor: pc latest

--- scripts/ball/ball-rigid.gd
+++ scripts/ball/ball-rigid.gd
@@ -12,26 +12,6 @@ func _ready() -> void:
 	player = get_node("../Player")
 
 
-func move():
-	if (owner_name != ""):
-		if is_instance_valid(player):
-			if (!ballKicked):
-				var offset: Vector2 = player.global_position
-				offset.y += 2;
-				if (player.movementDirection == "up"):
-					offset.y -= 6;
-					self.z_index = 1;
-				elif (player.movementDirection == "right"):
-					offset.x += 5;
-				elif (player.movementDirection == "down"):
-					offset.y += 3;
-				elif (player.movementDirection == "left"):
-					offset.x -= 5;
-				else:
-					offset.x += 6;
-				self.global_position = offset
-
-
 func kickCheck(delta):
 	if (ballKicked):
 			owner_name = ""
@@ -42,14 +22,29 @@ func kickCheck(delta):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move()
 	kickCheck(delta)
 	if (Input.is_action_pressed("switch")):
 		owner_name = ""
 
 
 func _on_body_entered(body: CharacterBody2D) -> void:
+	print(ballKicked);
 	if owner_name == "":
-		ballKicked = false
-		#self.velocity = Vector2(0, 0)
 		owner_name = body.player_name
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if (owner_name == "kev" && ballKicked == false):
+		self.sleeping = true
+		if (player.movementDirection == "up"):
+			
+			self.transform.origin.y = player.transform.origin.y - 6;
+			#self.z_index = 1;
+		elif (player.movementDirection == "right"):
+			
+			self.transform.origin.x = player.transform.origin.x + 5;
+		elif (player.movementDirection == "down"):
+			
+			self.transform.origin.y = player.transform.origin.y + 3;
+		elif (player.movementDirection == "left"):
+			
+			self.transform.origin.x = player.transform.origin.x - 5;
